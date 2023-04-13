61591fbb47f8776b31698d7e4737327009d36de8
Gian Barzano <gianbarzano@gmail.com>
2023-04-12 21:32:10 -0300

    add example page and not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Page2Component } from './page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'page-2',
+		component: Page2Component
+	},
+	{
+		path: '**',
+		component: PageNotFoundComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
